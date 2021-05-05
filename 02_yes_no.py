4103479b52d556756780330730473c209baca1c4
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-05-05 14:03:10 +1200

    Initial commit

---
+++ 02_yes_no.py
@@ -0,0 +1,23 @@
+# Checks that the user has entered yes / no to a question
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
+
+    
+# Loop to make testing faster...
+for item in range(0,6):
+    want_help = yes_no("Do you want to read the instructions? ")
+    print("You said '{}'\n".format(want_help))
