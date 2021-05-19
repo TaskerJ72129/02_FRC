93b8110d7a2644c6c1c94b6f52e64937f94f2ee3
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-05-19 14:14:15 +1200

    05

--- 05_profit_goal.py
+++ 05_profit_goal.py
@@ -1,68 +1,23 @@
-# import libraries
-import pandas
-
 # ---- Functions go here ----
 
 # Checks that the user has entered yes / no to a question
 def yes_no(question):
 
-    # Set up dictionaries and lists
+    to_check = ["yes", "no"]
 
-    item_list = []
-    quantity_list = []
-    price_list = []
+    valid = False
+    while not valid:
 
-    variable_dict = {
-        "Item": item_list,
-        "Quantity": quantity_list,
-        "Price": price_list
-    }
+        response = input(question).lower()
 
-    # loop to get component, quantity and price
-    item_name = ""
-    while item_name.lower() != "xxx":
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
 
-        print()
-        # get name, quantity and item
-        item_name = not_blank("Item name: ",
-                              "The component name can't be "
-                              "blank.")
-        if item_name.lower() == "xxx":
-            break
+        print("Please enter either yes or no...\n")
 
-        if var_fixed == "variable":
-            quantity = num_check("Quantity:",
-                                 "The amount must be a whole number more than 0",
-                                 int)
-        else:
-            quantity = 1
-
-        price = num_check("How much for a single item? $",
-                          "The price must be a number <more than 0>",
-                          float)
-
-        # add item, quantity and price to lists
-        item_list.append(item_name)
-        quantity_list.append(quantity)
-        price_list.append(price)
-
-
-    expense_frame = pandas.DataFrame(variable_dict)
-    expense_frame = expense_frame.set_index('Item')
-
-    # Calculate cost of each component
-    expense_frame['Cost'] = expense_frame['Quantity']\
-                            * expense_frame['Price']
-
-    # Find sub total
-    sub_total = expense_frame['Cost'].sum()
-
-    # Currency Formatting (uses currency function)
-    add_dollars = ['Price', 'Cost']
-    for item in add_dollars:
-        expense_frame[item] = expense_frame[item].apply(currency)
-
-    return [expense_frame, sub_total]
 
 def profit_goal(total_costs):
 
@@ -82,5 +37,58 @@ def profit_goal(total_costs):
             amount = response[1:]
 
         # check if thr last character is %
-        elif response [1] == "%":
+        elif response [-1] == "%":
             profit_type = "$"
+            # Get amount (everything before the %)
+            amount = response[:-1]
+
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # Check amount is a number more that zero
+            amount = float(amount)
+            if amount <= 0:
+                print(error)
+                continue
+
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unknown" and amount >= 100:
+            dollar_type = yes_no("Do you mean ${:.2f}.  "
+                                 "ie {:.2f} dollars? , y / n ".format(amount, amount))
+
+            # Set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif  profit_type == "unknown" and amount < 100:
+            percent_type = yes_no("Do you mean {}%? , y / n ".format(amount))
+            if percent_type == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        # return profit goal to main routine
+        if profit_type == "$":
+            return amount
+        else:
+            goal = (amount / 100) * total_costs
+            return goal
+
+
+# Main routine goes here
+all_costs = 200
+
+# Loop for quick testing
+for item in range(0, 6):
+    profit_target = profit_goal(all_costs)
+    print("Profit Target: ${:.2f}".format(profit_target))
+    print("Total Sales: ${:.2f}".format(all_costs + profit_target))
+    print()
